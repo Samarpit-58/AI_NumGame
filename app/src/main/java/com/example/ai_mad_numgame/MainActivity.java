70d59e48f8a2adc4c5eb75153e823ce40e878495
Samampit Gupta <201b337@juetguna.in>
2022-06-03 18:55:47 +0530

    Version

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         int[][]dataFrame=dataPrep(); //dataPrep function returns a two-dimenssional array
         double slope=LR.getSlope(dataFrame); //LR class, which provides slope on invoking getSlope
         new AlertDialog.Builder(this)
-               // .setIcon() //your custom icon
+                // .setIcon() //your custom icon
                 .setTitle("Performance")
 
                 .setMessage(getInterpretation(dataFrame,slope))
@@ -72,13 +72,50 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
+        int operand2=random.nextInt(10);
+        int correctAnswer=-100;
         //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
 
-      // Your code here, to diplay correct and incorrect options on the buttons
+        // Your code here, to diplay correct and incorrect options on the buttons
+        if(operator.equals("+")){
+            correctAnswer=operand1+operand2;
 
+        }
+        else if(operator.equals("-")){
+            correctAnswer=operand1-operand2;
+        }else if(operator.equals("*")){
+            correctAnswer=operand1*operand2;
+        }else if(operator.equals("/")){
+            correctAnswer=operand1/operand2;
+        }
+
+        correctButton = random.nextInt(4);
+        if(correctButton==0){
+            button1.setText(correctAnswer+" ");
+            button2.setText(correctAnswer-1+"");
+            button3.setText(correctAnswer+1+"");
+            button4.setText(correctAnswer-2+"");
+        }
+        if(correctButton==3){
+            button4.setText(correctAnswer+" ");
+            button2.setText(correctAnswer-1+"");
+            button3.setText(correctAnswer+1+"");
+            button1.setText(correctAnswer-2+"");
+        }
+        if(correctButton==2){
+            button3.setText(correctAnswer+" ");
+            button2.setText(correctAnswer-1+"");
+            button1.setText(correctAnswer+1+"");
+            button4.setText(correctAnswer-2+"");
+        }
+        if(correctButton==1){
+            button2.setText(correctAnswer+" ");
+            button1.setText(correctAnswer-1+"");
+            button3.setText(correctAnswer+1+"");
+            button4.setText(correctAnswer-2+"");
+        }
         if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
 
             matchCounter=0;
@@ -95,7 +132,11 @@ public class MainActivity extends AppCompatActivity {
     public int sumOfScore(){
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
         int sum=0;
-       // your code here
+        // your code here\
+        for(int i= 0;i<score.length;i++){
+            sum+=score[i];
+
+        }
         return sum;
     }
 
@@ -113,8 +154,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String getInterpretation(int [][]dataFrame,double slope){
-       //provide interpretation based on your slope analysis
+        //provide interpretation based on your slope analysis
         // Your code here
+        if(slope>0&&slope<=0.5)
+            return "very bad";
+        if(slope>0.5)
+            return"very good";
         return "Your Interpretation";
     }
 }
